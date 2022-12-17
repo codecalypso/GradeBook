58ad5b4810bb418a9cd63db301fe5c8c7aa76474
codecalypso <smullins10@gmail.com>
2022-12-17 13:49:42 -0600

    Scaffold project

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          if(args.Length > 0)
+          {
+            Console.WriteLine($"Hello, {args[0]}!");
+          }
+          else
+          {
+            Console.WriteLine("Hello!");
+
+          }
+        }
+    }
+}
