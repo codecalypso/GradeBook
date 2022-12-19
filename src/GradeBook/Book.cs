ad2fdd002e629fc26b54e5a58cf65a29442c35dc
codecalypso <smullins10@gmail.com>
2022-12-18 22:36:17 -0600

    Create tests related to reference types & value types

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,36 +1,36 @@
 namespace GradeBook
 {
-    class Book 
+    public class Book 
     {
       //constructor
       public Book(string name)
       {
         grades = new List<double>();
-        this.name = name;
+        Name = name;
       }
       public void AddGrade(double grade)
       {
         grades.Add(grade);
       }
       
-      public void showStaistics()
+      public Statistics GetStatistics()
       {
-        var result = 0.0;
-         var highGrade = double.MinValue;
-         var lowGrade = double.MaxValue;
-         foreach(var number in grades)
+         var result = new Statistics();
+         result.Average = 0.0;
+         result.High = double.MinValue;
+         result.Low = double.MaxValue;
+         foreach(var grade in grades)
          {
-          lowGrade = Math.Min(number, lowGrade);
-          highGrade = Math.Max(number, highGrade);
-          result += number;
+          result.Low = Math.Min(grade, result.Low);
+          result.High = Math.Max(grade, result.High);
+          result.Average += grade;
          }
-         result/=grades.Count;
-         Console.WriteLine($"The lowest grade is {lowGrade}");
-         Console.WriteLine($"The highest grade is {highGrade}");
-         Console.WriteLine($"The average grade is {result:N1}");
+         result.Average/=grades.Count;
+         return result;
+       
       }
       //state
       List<double> grades;
-      private string name;
+      public string Name;
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,7 +10,12 @@ namespace GradeBook
          book.AddGrade(89.1);
          book.AddGrade(60.0);
          book.AddGrade(100.0);
-         book.showStaistics();
+
+         var stats = book.GetStatistics();
+         
+         Console.WriteLine($"The lowest grade is {stats.Low}");
+         Console.WriteLine($"The highest grade is {stats.High}");
+         Console.WriteLine($"The average grade is {stats.Average:N1}");
         }
     }
 }
