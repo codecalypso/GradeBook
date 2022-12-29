5969ac68e646ce8a85754ffb95e8ba8e00349456
codecalypso <smullins10@gmail.com>
2022-12-29 09:56:17 -0600

    C# OOP

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,92 +1,128 @@
 namespace GradeBook
 {
     public delegate void GradeAddedDelegate(object sender, EventArgs args);
-    public class Book 
-    {
-      //constructor
-      public Book(string name)
-      {
-        grades = new List<double>();
-        Name = name;
-      }
-       public void AddLetterGrade(char letter)
-      {
-        switch(letter)
-        {
-           case 'A':
-              AddGrade(90);
-              break;
-            case 'B':
-              AddGrade(80);
-              break; 
-            case 'C':
-              AddGrade(70);
-              break; 
-            default:
-              AddGrade(0);
-              break;
-        }
-      }
-      public void AddGrade(double grade)
-      {
-        if(grade <= 100 && grade >= 0)
-        {
-          grades.Add(grade);
-          if(GradeAdded != null)
-          {
-            GradeAdded(this, new EventArgs());
-          }
-        }
-        else
-        {
-          throw new ArgumentException($"Invalid {nameof(grade)}");
-        }
-      }
-      public event GradeAddedDelegate GradeAdded;
-      public Statistics GetStatistics()
-      {
-         var result = new Statistics();
-         result.Average = 0.0;
-         result.High = double.MinValue;
-         result.Low = double.MaxValue;
-         
-         for(var index = 0; index < grades.Count; index+=1)
-         {
-          result.Low = Math.Min(grades[index], result.Low);
-          result.High = Math.Max(grades[index], result.High);
-          result.Average += grades[index];
-         };
-         result.Average/=grades.Count;
 
-         //pattern matching
-         switch(result.Average)
-         {
-            case var d when d >= 90.0:
-              result.Letter = 'A';
-              break;
-            case var d when d >= 80.0:
-              result.Letter = 'B';
-              break;
-            case var d when d >= 70.0:
-              result.Letter = 'C';
-              break;
-            case var d when d >= 60.0:
-              result.Letter = 'D';
-              break;
-            default:
-              result.Letter = 'F';
-              break;
-         }
-         return result;
-       
-      }
-      //state
-      List<double> grades;
-      public string Name
-      {
-        get;
-        set;
-      }
-      public const string CATEGORY = "Science";
+    //base class
+    public class NamedObject
+    {
+        public NamedObject(string name)
+        {
+            Name = name;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+
+    public interface IBook
+    {
+        void AddGrade(double grade);
+        Statistics GetStatistics();
+        string Name { get; }
+        event GradeAddedDelegate GradeAdded;
+    }
+    public abstract class Book : NamedObject, IBook
+    {
+        public Book(string name) : base(name)
+        {
+        }
+
+        public abstract event GradeAddedDelegate GradeAdded;
+
+        public abstract void AddGrade(double grade);
+
+        public abstract Statistics GetStatistics();
+
+    }
+    public class DiskBook : Book
+    {
+        public DiskBook(string name) : base(name)
+        {
+        }
+
+        public override event GradeAddedDelegate GradeAdded;
+
+        public override void AddGrade(double grade)
+        {
+            using (var writer = File.AppendText($"{Name}.txt"))
+            {
+                writer.WriteLine(grade);
+                if (GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+        }
+
+        public override Statistics GetStatistics()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class InMemoryBook : Book
+    {
+        //constructor
+        public InMemoryBook(string name) : base(name) //accesses base class
+        {
+            grades = new List<double>();
+            Name = name;
+        }
+        public void AddLetterGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+        public override void AddGrade(double grade)
+        {
+            if (grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+                if (GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
+        }
+        public override event GradeAddedDelegate GradeAdded;
+        public override Statistics GetStatistics()
+        {
+            var result = new Statistics();
+
+            using (var reader = File.OpenText($"{Name}.txt"))
+            {
+                var line = reader.ReadLine();
+                while (line != null)
+                {
+                    var number = double.Parse(line);
+                    result.Add(number);
+                    line = reader.ReadLine();
+                }
+            }
+
+            return result;
+        }
+        //state
+        List<double> grades;
+
+        public const string CATEGORY = "Science";
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,47 +6,50 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-         var book = new Book("Stacy's Grade Book");
-         book.GradeAdded += OnGradeAdded;
-         book.GradeAdded += OnGradeAdded;
-         book.GradeAdded -= OnGradeAdded;
-         book.GradeAdded += OnGradeAdded;
+            var book = new InMemoryBook("Stacy's Grade Book");
+            book.GradeAdded += OnGradeAdded;
 
-         while(true)
-         {
-          Console.WriteLine("Enter a grade or 'q' to quit");
-          var input = Console.ReadLine();
-          if(input == "q")
-          {
-            break;
-          }
-          try
-          {
-            var grade = double.Parse(input);
-            book.AddGrade(grade);
-          }
-          catch(ArgumentException ex)
-          {
-            Console.WriteLine(ex.Message);
-            throw;
-          }
-          catch(FormatException ex)
-          {
-            Console.WriteLine(ex.Message);
-            throw;
-          }
-          finally
-          {
-            Console.WriteLine("*");
-          }
-         }
+            EnterGrades(book);
 
-         var stats = book.GetStatistics();
-         Console.WriteLine(Book.CATEGORY);
-         Console.WriteLine($"The lowest grade is {stats.Low}");
-         Console.WriteLine($"The highest grade is {stats.High}");
-         Console.WriteLine($"The average grade is {stats.Average:N1}");
-         Console.WriteLine($"The letter grade is {stats.Letter}");
+            var stats = book.GetStatistics();
+            // Console.WriteLine(InMemoryBook.CATEGORY);
+            Console.WriteLine($"The name of the book is {book.Name}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+
+        private static void EnterGrades(IBook book)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+                if (input == "q")
+                {
+                    break;
+                }
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    throw;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    throw;
+                }
+                finally
+                {
+                    Console.WriteLine("*");
+                }
+            }
         }
 
         static void OnGradeAdded(object sender, EventArgs e)
